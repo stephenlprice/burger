b540bb753d3553a0224f174ce36a66a75ac2b690
Stephen Price <stephen_lprice@live.com>
2021-02-11 00:37:22 -0600

    added heroku connection to JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Connection setup
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'bootcamp',
-  database: 'burger_db',
-});
+const connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'bootcamp',
+    database: 'burger_db',
+  });
+}
 
 // Make the connection
 connection.connect((err) => {
